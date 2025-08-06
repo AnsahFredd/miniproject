ddd6748ab8c9406d98d34996ecb9fb17e11ef928
Ansah Fred <ansfred111@gmail.com>
2025-08-06 06:53:18 +0000

    Update gitignore and user crud

--- backend/app/crud/user_crud.py
+++ backend/app/crud/user_crud.py
@@ -6,4 +6,4 @@ async def get_user_by_email(email: str) -> User | None:
 async def update_user_password(user: User, hashed_password: str) -> User:
     user.hashed_password = hashed_password
     await user.save()
-    return user
+    return user  #reteun
